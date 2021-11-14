a0693ded7a2111751f3f40e81387f4790b51e8bc
mengshalitsofi <mengshalitsofi@gmail.com>
2021-11-14 04:03:17 -0800

    title change

--- family-calendar-frontend/src/App.js
+++ family-calendar-frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Family Calendar
+          Family Calendar!
         </p>                
       </header>
       <CalendarContainer />      
